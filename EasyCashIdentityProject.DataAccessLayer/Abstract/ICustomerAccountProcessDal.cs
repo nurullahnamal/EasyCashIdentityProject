27af552b40ce8a65a2da18c92f231ebb02eb44f7
nurullah namal <nurullahnamal12@hotmail.com>
2023-08-11 12:27:49 +0300

    9.commint

--- EasyCashIdentityProject.DataAccessLayer/Abstract/ICustomerAccountProcessDal.cs
+++ EasyCashIdentityProject.DataAccessLayer/Abstract/ICustomerAccountProcessDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DataAccessLayer.Abstract
 {
-    public interface ICustomerAccountProcessDal:IGenericDal<ICustomerAccountProcessDal>
+    public interface ICustomerAccountProcessDal : IGenericDal<CustomerAccountProcess>
     {
     }
 }
