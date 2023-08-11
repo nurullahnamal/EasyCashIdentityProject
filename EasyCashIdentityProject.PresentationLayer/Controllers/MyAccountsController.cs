2e7f3855e28fef93835b709136d5864bd521a9aa
nurullah namal <nurullahnamal12@hotmail.com>
2023-08-12 00:16:11 +0300

    22.commint

--- EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -20,7 +20,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
         public async Task<IActionResult> Index()
         {
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
-            AppUserEditDto appUserEditDto=new AppUserEditDto();
+            AppUserEditDto appUserEditDto = new AppUserEditDto();
             appUserEditDto.Name = values.Name;
             appUserEditDto.Surname = values.Surname;
             appUserEditDto.PhoneNumber = values.PhoneNumber;
@@ -29,7 +29,29 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             appUserEditDto.District = values.District;
             appUserEditDto.ImageUrl = values.ImageUrl;
             return View(appUserEditDto);
-           
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserEditDto appUserEditDto)
+        {
+            if (appUserEditDto.Password==appUserEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.PhoneNumber = appUserEditDto.PhoneNumber;
+                user.Surname = appUserEditDto.Surname;
+                user.City = appUserEditDto.City;
+                user.District = appUserEditDto.District;
+                user.Name = appUserEditDto.Name;
+                user.ImageUrl = "Test";
+                user.Email = appUserEditDto.Email;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user,appUserEditDto.Password);
+                var result=await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+            }
+            return View();
+
         }
     }
 }
