3e06b70e742e4d65f54bdb254e474fb664810656
nurullah namal <nurullahnamal12@hotmail.com>
2023-08-11 18:56:09 +0300

    18.commint

--- EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/ConfirmMailController.cs
@@ -34,7 +34,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             {
                 user.EmailConfirmed = true;
                 await _userManager.UpdateAsync(user);
-                return RedirectToAction("Index","MyProfile");
+                return RedirectToAction("Index","Login");
             }
             return View();
         }
