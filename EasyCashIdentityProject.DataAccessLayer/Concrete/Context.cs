7413d9ba042a0e6999f9f2140e9fa8bd3872987c
nurullah namal <nurullahnamal12@hotmail.com>
2023-08-12 11:01:34 +0300

    23.commint

--- EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -9,14 +9,33 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DataAccessLayer.Concrete
 {
-    public class Context:IdentityDbContext<AppUser,AppRole,int>
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"server=DESKTOP-CHMVP03\SQLEXPRESS;initial catalog=EasyCashDb;integrated Security=true");
-            
+
         }
-        public DbSet<CustomerAccount> CustomerAccounts{ get; set; }
-        public DbSet<CustomerAccountProcess> CustomerAccountProcesses{ get; set; }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CustomerAccountProcess>()
+                .HasOne(x => x.SenderCustomer)
+                .WithMany(y => y.CustomerSender)
+                .HasForeignKey(z => z.SenderID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.Entity<CustomerAccountProcess>()
+                .HasOne(x => x.ReceiverCustomer)
+                .WithMany(y => y.CustomerReceiver)
+                .HasForeignKey(z => z.ReceiverID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+            base.OnModelCreating(builder);
+
+
+        }
     }
 }
